e27909bf398371c17f00a28882e1a2e30ff1a323
Mesut Turgut <mes_tur@hotmail.com>
2020-10-01 00:51:17 +0300

    xx
    xx

--- src/reducers/authUser.js
+++
@@ -1,8 +0,0 @@
-import { SET_AUTHED_USER } from '../actions/authUser';
-
-export default function authUser(state = null, action) {
-  if (action.type === SET_AUTHED_USER) {
-    return action.id;
-  }
-  return state;
-}
